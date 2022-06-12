0e4ddfcefc407a66f4380e004a921da7c1e54eb2
Vivek Kumar <vivek32032@gmail.com>
2022-06-12 16:48:33 +0530

    filter users from db

--- block-BMaaam/MongoAggregation/routes/users.js
+++ block-BMaaam/MongoAggregation/routes/users.js
@@ -4,22 +4,74 @@ var User = require('../models/User');
 
 /* GET users listing. */
 router.get('/active', function(req, res, next) {
-  // Users with 'Blake' in their name
+
+  // 1.active users 
   User.aggregate([
     {
       $match: {
-        $text: {
-          $search: 'blake'
-        }
+        isActive: true,
       }
     }
   ], (err, users) => {
     if (err) return next(err);
-    console.log(users);     
+    console.log(users.length);     
     res.render('shortedUsers', { users });
   })
+  
+  // 2.Users with 'Blake' in their name
 
+  // User.aggregate([
+  //   {
+  //     $match: {
+  //       $text: {
+  //         $search: 'blake'
+  //       }
+  //     }
+  //   }
+  // ], (err, users) => {
+  //   if (err) return next(err);
+  //   console.log(users);     
+  //   res.render('shortedUsers', { users });
+  // })
+
+   // 3.all active males 
+  //  User.aggregate([
+  //   {
+  //     $match: {
+  //       isActive: true,
+  //       gender: "male",
+
+  //     }
+  //   }
+  // ], (err, users) => {
+  //   if (err) return next(err);
+  //   console.log(users.length);     
+  //   res.render('shortedUsers', { users });
+  // })
+
+  // 6. Find all 40+ males with green eyecolor
+ 
+  // User.aggregate([
+  //   { 
+  //     $match: 
+  //     { 
+  //       eyeColor: 'green',
+  //       gender: 'male',
+  //       age: {
+  //         $gt: 35
+  //       }
+  //        }
+  //     }
+    
+  // ], (err, users) => {
+  //   if (err) return next(err);
+  //   console.log(users.length);     
+  //   res.render('shortedUsers', { users });
+  // })
+
+  
   // Men with blue eyes working in USA 
+  
   // User.aggregate([
   //   { 
   //     $match: 
